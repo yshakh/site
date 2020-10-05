183a66a9915eec61500ef2b4ab689529b23a1cb8
Vaisakh Unnithan <vunnithan@athenahealth.com>
2020-10-06 01:40:49 +0530

    Updates

---
+++ precache-manifest.c0cdad12b10299869657bae12d73d070.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680f40772df13244561802f7f048c675",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "464cb17c62f417752fb5",
+    "url": "/site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8d131a415d643879de4",
+    "url": "/site/static/js/2.712263c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/site/static/js/2.712263c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464cb17c62f417752fb5",
+    "url": "/site/static/js/main.33561623.chunk.js"
+  },
+  {
+    "revision": "de9d1538aef45f6e5823",
+    "url": "/site/static/js/runtime-main.7d3ed058.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/site/static/media/logo.5d5d9eef.svg"
+  }
+]);
